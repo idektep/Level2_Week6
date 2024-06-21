3180111922f1788a0deb5d6ec0edfb88c7377e39
idektep <idektep.coach@gmail.com>
2024-06-21 15:31:30 +0700

    Add files via upload

---
+++ LAB_LCD/LAB_LCD.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // 0x27 or 0x3F
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin();
+  // Turn on the blacklight and print a message.
+  lcd.backlight();  //เปิดไฟ backlight
+  lcd.setCursor(1, 0);
+  lcd.print("Idektep Cobot");
+  lcd.setCursor(3, 1);
+  lcd.print("Name");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
