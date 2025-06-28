850a68da06e570ae62820ce8143ffde860dc8c9f
idektep <idektep.coach@gmail.com>
2025-06-28 11:41:38 +0700

    Update LAB_LCD_DHT_add.ino

--- LAB_LCD_DHT_add/LAB_LCD_DHT_add.ino
+++ LAB_LCD_DHT_add/LAB_LCD_DHT_add.ino
@@ -13,13 +13,17 @@ void loop() {
   lcd.setCursor(0, 0);
   lcd.print("x");
   delay(1000);
+  lcd.clear();
   lcd.setCursor(1, 0);
   lcd.print("x");
   delay(1000);
+  lcd.clear();
   lcd.setCursor(2, 0);
   lcd.print("x");
   delay(1000);
+  lcd.clear();
   lcd.setCursor(3, 0);
   lcd.print("x");
   delay(1000);
+  lcd.clear();
 }
